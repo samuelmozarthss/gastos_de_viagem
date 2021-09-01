f090960d25cd7dae414074cfc23f54b37a9fd5ef
Samuel Mozarth <65967088+samuelmozarthss@users.noreply.github.com>
2021-09-01 11:52:08 -0300

    primeiro commit app gastos de viagem

---
+++ app/src/main/java/com/example/gastosdeviagem/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.gastosdeviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        supportActionBar!!.hide()
+
+        buttonCalculate.setOnClickListener {
+            
+        }
+    }
+
+    fun teste(view: android.view.View) {}
+}
