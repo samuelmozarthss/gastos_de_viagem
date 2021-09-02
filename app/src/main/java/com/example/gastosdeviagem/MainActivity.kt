9b6948d55262cd9e766e18a7b0bd1d20bca1e5ef
Samuel Mozarth <65967088+samuelmozarthss@users.noreply.github.com>
2021-09-02 01:25:51 -0300

    app pronto

--- app/src/main/java/com/example/gastosdeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastosdeviagem/MainActivity.kt
@@ -2,18 +2,58 @@ package com.example.gastosdeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //        supportActionBar!!.hide()
 
-        buttonCalculate.setOnClickListener {
-            
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if (id == R.id.buttonCalculate) {
+            calculate()
         }
     }
 
-    fun teste(view: android.view.View) {}
+    private fun calculate() {
+
+        if (validationOk()) {
+
+            try {
+
+                val distance = valueDistance.text.toString().toFloat()
+                val price = valuePrice.text.toString().toFloat()
+                val autonomy = valueAutonomy.text.toString().toFloat()
+
+                val totalValue = (distance * price) / autonomy
+                textTotalValues.text = "R$ ${"%.2f".format(totalValue)}"
+
+            } catch (execoes: NumberFormatException) {
+
+                Toast.makeText(this, getString(R.string.notificcampos), Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+
+        Toast.makeText(this, getString(R.string.valoresValidos), Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun validationOk(): Boolean = (
+        valueDistance.text.toString() != "" &&
+        valueAutonomy.text.toString() != "" &&
+        valuePrice.text.toString()    != "" &&
+        valuePrice.text.toString()    != "0" &&
+        valueDistance.text.toString() != "0" &&
+        valueAutonomy.text.toString() != "0")
+
 }
